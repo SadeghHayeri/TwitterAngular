48cdc85ba6bf94baa653e9cd3610a4ddc18a5491
SadeghHayeri <hayerisadegh@gmail.com>
2017-07-09 12:04:20 +0430

    clean project!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
   ],
   imports: [
     BrowserModule,
