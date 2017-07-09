71acb301ce65c22d6e6bc7a88cbb1cdbbc65c092
SadeghHayeri <hayerisadegh@gmail.com>
2017-07-10 00:15:18 +0430

    new tweet added!

---
+++ src/app/new-tweet/new-tweet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-tweet',
+  templateUrl: './new-tweet.component.html',
+  styleUrls: ['./new-tweet.component.css']
+})
+export class NewTweetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
