ab7b28f229572d0898ce605a9c25c6a3baa7c57f
SadeghHayeri <hayerisadegh@gmail.com>
2017-07-10 00:16:33 +0430

    main component edited!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewTweetComponent } from './new-tweet/new-tweet.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
+import { TweetComponent } from './tweet/tweet.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    NewTweetComponent,
+    ProfileInfoComponent,
+    TweetComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
